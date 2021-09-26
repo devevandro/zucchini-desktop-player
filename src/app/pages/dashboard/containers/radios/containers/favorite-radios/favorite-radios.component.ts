5adcf518d64d4a77aa92ee41be98598d0146fa2b
devevandro <dev.evandro@gmail.com>
2021-09-26 11:12:30 -0300

    Cria componente p/ radios favoritas

---
+++ src/app/pages/dashboard/containers/radios/containers/favorite-radios/favorite-radios.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-radios',
+  templateUrl: './favorite-radios.component.html',
+  styleUrls: ['./favorite-radios.component.scss']
+})
+export class FavoriteRadiosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {}
+}
