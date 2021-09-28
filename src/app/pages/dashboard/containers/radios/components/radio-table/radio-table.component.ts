9cebd89c5f805a11494b491bf841def3edda6e90
devevandro <dev.evandro@gmail.com>
2021-09-27 21:06:36 -0300

    Adiciona tabela p/ listar as rádios

--- src/app/pages/dashboard/containers/radios/components/radio-table/radio-table.component.ts
+++ src/app/pages/dashboard/containers/radios/components/radio-table/radio-table.component.ts
@@ -1,5 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { State } from '@models/state.interface';
 import { Radio } from '@models/radio.interface';
@@ -12,17 +13,31 @@ import { Radio } from '@models/radio.interface';
 export class RadioTableComponent implements OnInit {
   public state: State;
   public radios: Radio[] = [];
+  public stateName = '';
 
   constructor(
+    private router: Router,
     private location: Location,
   ) { }
 
   ngOnInit(): void {
     const { datas } = history.state;
-    this.state = datas;
-    const { content } = this.state;
-    this.radios = content;
-    console.log(this.radios);
+
+    if (datas === undefined) {
+      this.router.navigate(['/regiao-sul']);
+    } else {
+      if (datas) {
+        this.state = datas;
+        const { content, name } = this.state;
+        this.radios = content;
+        this.stateName = name;
+        console.log(this.radios);
+      }
+    }
+  }
+
+  handlePlay(radio: Radio): void {
+    console.log(radio);
   }
 
   handleBack(): void {
