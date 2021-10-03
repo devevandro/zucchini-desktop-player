e7c17999b33f6eaca0c6e5fae55e0d65d5b11d0f
devevandro <dev.evandro@gmail.com>
2021-10-03 08:30:19 -0300

    Refatora código para executar as rádios

--- src/app/pages/dashboard/containers/radios/components/radio-table/radio-table.component.ts
+++ src/app/pages/dashboard/containers/radios/components/radio-table/radio-table.component.ts
@@ -4,27 +4,28 @@ import { Router } from '@angular/router';
 
 import { State } from '@models/state.interface';
 import { Radio } from '@models/radio.interface';
+import { RadioPlayService } from 'src/app/services/radio-play.service';
 
 @Component({
   selector: 'app-radio-table',
   templateUrl: './radio-table.component.html',
-  styleUrls: ['./radio-table.component.scss']
+  styleUrls: ['./radio-table.component.scss'],
 })
 export class RadioTableComponent implements OnInit {
   public state: State;
   public radios: Radio[] = [];
   public radio: Radio;
   public stateName = '';
-  public audioObj = new Audio();
-  public radioPlaying = '';
   public isPlaying = false;
   public isMute = false;
-  public volume = 50;
+  public volume = 30;
+  public color = '#474a51';
 
   constructor(
     private router: Router,
     private location: Location,
-  ) { }
+    private radioPlay: RadioPlayService
+  ) {}
 
   ngOnInit(): void {
     const { datas } = history.state;
@@ -42,41 +43,39 @@ export class RadioTableComponent implements OnInit {
   }
 
   handlePlay(radio: Radio = this.radio): void {
-    this.radio =  radio;
-    const { url } = radio;
-    this.audioObj.src = url;
-    this.audioObj.play();
-    this.radioPlaying = url;
+    this.radio = radio;
+    this.radioPlay.play(radio);
     this.isPlaying = true;
   }
 
   handlePause(): void {
-    this.audioObj.pause();
-    this.radioPlaying = '';
+    this.radioPlay.pause();
     this.isPlaying = false;
   }
 
   handleUp(): void {
-    this.audioObj.play();
-    this.audioObj.volume = 1;
+    this.radioPlay.up();
     this.isMute = false;
-    this.volume = 50;
+    this.volume = 30;
   }
 
   handleMute(): void {
-    this.audioObj.volume = 0;
+    this.radioPlay.mute();
     this.isMute = true;
     this.volume = 0;
   }
 
   onBack(): void {
-    this.audioObj.pause();
-    this.radioPlaying = '';
+    this.radioPlay.pause();
+
+    if (this.radio) {
+      this.radio.url = '';
+    }
     this.location.back();
   }
 
-  setVolume(): void {
-    this.audioObj.volume = this.volume;
+  setVolume(volume: number = this.volume): void {
+    this.radioPlay.setVolume(volume);
 
     if (this.volume === 0) {
       this.isMute = true;
