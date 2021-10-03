2305ec00cdca3dbcd94efa8ddd293e0de3fe5ae5
devevandro <dev.evandro@gmail.com>
2021-10-03 08:29:10 -0300

    Cria service para executar as rádios

---
+++ src/app/services/radio-play.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+import { Radio } from '@models/radio.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RadioPlayService {
+  public audioObj = new Audio();
+
+  constructor() {}
+
+  play(radio: Radio): void {
+    const { url } = radio;
+    this.audioObj.src = url;
+    this.audioObj.play();
+  }
+
+  pause(): void {
+    this.audioObj.pause();
+  }
+
+  up(): void {
+    this.audioObj.play();
+    this.audioObj.volume = 1;
+  }
+
+  mute(): void {
+    this.audioObj.volume = 0;
+  }
+
+  setVolume(volume: number): void {
+    this.audioObj.volume = volume / 100;
+  }
+}
