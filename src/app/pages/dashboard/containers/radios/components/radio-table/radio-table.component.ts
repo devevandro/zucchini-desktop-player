6fa80d6f6504151c9583486636f9f7b1cab6ff8e
devevandro <dev.evandro@gmail.com>
2021-10-02 22:17:20 -0300

    Configura controles para tocar as rádios

--- src/app/pages/dashboard/containers/radios/components/radio-table/radio-table.component.ts
+++ src/app/pages/dashboard/containers/radios/components/radio-table/radio-table.component.ts
@@ -1,5 +1,5 @@
-import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
 import { State } from '@models/state.interface';
@@ -16,6 +16,10 @@ export class RadioTableComponent implements OnInit {
   public radio: Radio;
   public stateName = '';
   public audioObj = new Audio();
+  public radioPlaying = '';
+  public isPlaying = false;
+  public isMute = false;
+  public volume = 50;
 
   constructor(
     private router: Router,
@@ -33,19 +37,51 @@ export class RadioTableComponent implements OnInit {
         const { content, name } = this.state;
         this.radios = content;
         this.stateName = name;
-        console.log(this.radios);
       }
     }
   }
 
-  handlePlay(radio: Radio): void {
+  handlePlay(radio: Radio = this.radio): void {
     this.radio =  radio;
     const { url } = radio;
     this.audioObj.src = url;
     this.audioObj.play();
+    this.radioPlaying = url;
+    this.isPlaying = true;
   }
 
-  handleBack(): void {
+  handlePause(): void {
+    this.audioObj.pause();
+    this.radioPlaying = '';
+    this.isPlaying = false;
+  }
+
+  handleUp(): void {
+    this.audioObj.play();
+    this.audioObj.volume = 1;
+    this.isMute = false;
+    this.volume = 50;
+  }
+
+  handleMute(): void {
+    this.audioObj.volume = 0;
+    this.isMute = true;
+    this.volume = 0;
+  }
+
+  onBack(): void {
+    this.audioObj.pause();
+    this.radioPlaying = '';
     this.location.back();
+  }
+
+  setVolume(): void {
+    this.audioObj.volume = this.volume;
+
+    if (this.volume === 0) {
+      this.isMute = true;
+    } else {
+      this.isMute = false;
+    }
   }
 }
