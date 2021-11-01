cfaf2796c1584bad28a7f9a44a8bbedb13158db0
devevandro <dev.evandro@gmail.com>
2021-11-01 19:59:29 -0300

    Cria componentes

---
+++ src/app/pages/dashboard/components/radio-play/radio-play.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-radio-play',
+  templateUrl: './radio-play.component.html',
+  styleUrls: ['./radio-play.component.scss']
+})
+export class RadioPlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
