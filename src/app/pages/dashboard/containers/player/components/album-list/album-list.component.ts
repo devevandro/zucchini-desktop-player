0cc46530acc3f6e94a72d09177a1271185b4ee78
devevandro <dev.evandro@gmail.com>
2021-11-02 14:55:08 -0300

    Cria componente para listar os álbuns

--- src/app/pages/dashboard/containers/player/components/album-list/album-list.component.ts
+++ src/app/pages/dashboard/containers/player/components/album-list/album-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-album-list',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-list.component.scss']
 })
 export class AlbumListComponent implements OnInit {
+  @Input() albums = [];
 
   constructor() { }
 
