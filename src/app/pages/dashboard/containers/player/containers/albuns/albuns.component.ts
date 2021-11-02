5537a64954e62111b7ddca428fe8eff174c61e17
devevandro <dev.evandro@gmail.com>
2021-11-02 18:35:35 -0300

    Cria tab para o from. de playlist

--- src/app/pages/dashboard/containers/player/containers/albuns/albuns.component.ts
+++ src/app/pages/dashboard/containers/player/containers/albuns/albuns.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { Observable, Subscription } from 'rxjs';
 import { Select, Store } from '@ngxs/store';
+import { Observable, Subscription } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { ActionsExecuting, actionsExecuting } from '@ngxs-labs/actions-executing';
 
 import { Playlist } from '@models/playlist-model';
 import { PlaylistState } from '@store/playlist/playlist.state';
-import { LoadRequestPlaylists } from '@store/playlist/playlist.actions';
+import { CreatePlaylist, LoadRequestPlaylists } from '@store/playlist/playlist.actions';
 
 
 @Component({
@@ -15,23 +16,26 @@ import { LoadRequestPlaylists } from '@store/playlist/playlist.actions';
   styleUrls: ['./albuns.component.scss']
 })
 export class AlbunsComponent implements OnInit, OnDestroy {
-  @Select(actionsExecuting([LoadRequestPlaylists])) loading$: Observable<ActionsExecuting>;
+  @Select(actionsExecuting([CreatePlaylist, LoadRequestPlaylists])) loading$: Observable<ActionsExecuting>;
 
   public albums: Playlist[];
   public title = 'Álbuns';
   public newAlbum = 'Novo álbum';
   public subscription: Subscription[] = [];
 
-  constructor(private store: Store) { }
+  constructor(private store: Store, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.store.dispatch(new LoadRequestPlaylists());
 
     const loadingSubscription = this.loading$.subscribe(hasActionsExecuting => {
       if (hasActionsExecuting) {
+        this.spinner.show();
         return;
       }
 
+      this.spinner.hide();
+      this.store.dispatch(new LoadRequestPlaylists());
       this.albums = this.store.selectSnapshot(PlaylistState.getPlaylists);
     });
 
@@ -40,5 +44,9 @@ export class AlbunsComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscription.forEach(subscription => subscription.unsubscribe());
+  }
+
+  handleCreatePlaylist(playlist: Playlist): void {
+    this.store.dispatch(new CreatePlaylist(playlist));
   }
 }
