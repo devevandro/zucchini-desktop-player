4c9096f3cd9e21cd418a43793c7f6e931b0caef5
devevandro <dev.evandro@gmail.com>
2021-11-02 21:41:37 -0300

    Adiciona método para remover playlist

--- src/app/pages/dashboard/containers/player/components/album-list/album-list.component.ts
+++ src/app/pages/dashboard/containers/player/components/album-list/album-list.component.ts
@@ -1,16 +1,62 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+
+import { Select, Store } from '@ngxs/store';
+import { Observable, Subscription } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
+import { ActionsExecuting, actionsExecuting } from '@ngxs-labs/actions-executing';
+
+import { Playlist } from '@models/playlist-model';
+import { LoadRequestPlaylists, RemovePlaylistById } from '@store/playlist/playlist.actions';
 
 @Component({
   selector: 'app-album-list',
   templateUrl: './album-list.component.html',
   styleUrls: ['./album-list.component.scss']
 })
-export class AlbumListComponent implements OnInit {
+export class AlbumListComponent implements OnInit, OnDestroy {
   @Input() albums = [];
+  @Select(actionsExecuting([RemovePlaylistById])) loading$: Observable<ActionsExecuting>;
 
-  constructor() { }
+  public subscription: Subscription[] = [];
+  public confirmModal?: NzModalRef;
+
+  constructor(
+    private store: Store,
+    private modal: NzModalService,
+    private spinner: NgxSpinnerService,
+  ) { }
 
   ngOnInit(): void {
+    const loadingSubscription = this.loading$.subscribe(hasActionsExecuting => {
+      if (hasActionsExecuting) {
+        this.spinner.show();
+        return;
+      }
+
+      this.spinner.hide();
+    });
+
+    this.subscription.push(loadingSubscription);
   }
 
+  ngOnDestroy(): void {
+    this.subscription.forEach(subscription => subscription.unsubscribe());
+  }
+
+  handleRemovePlaylist(playlist: Playlist): void {
+    const { _id, name} = playlist;
+    this.confirmModal = this.modal.confirm({
+      nzTitle: `Excluir a playlist.`,
+      nzOkText: 'Remover',
+      nzContent: `Deseja remover a playlist ${name}? Esta ação não poderá ser desfeita.`,
+      nzOkType: 'danger',
+      nzOnOk: () => this.handleRemove(_id),
+      nzOnCancel: () => {},
+    });
+  }
+
+  handleRemove(id: string): void {
+    this.store.dispatch(new RemovePlaylistById(id));
+  }
 }
